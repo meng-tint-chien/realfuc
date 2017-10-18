a0aacf70ff0b00f6289677670c71532ae4bee2bd
first <31497704+meng-tint-chien@users.noreply.github.com>
2017-10-18 19:00:34 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ access_token = 'YOUR_IMGUR_ACCESS_TOKEN'
 refresh_token = 'YOUR_IMGUR_ACCESS_TOKEN'
 
 # line bot key
-line_channel_access_token = 'y9AC19uX8VOLcgpFZpJRjx2v9LJ9aDSUCgvHZhnhdijtDbSKhvcayE9hPRwFlCRjUvMVPCZYox1rYMwaekLeEVyJ0gDv9cTA0dGdRyigKk5Qjos+gwUDsxI2H9IP7SpgfKyGmakdqUpI+uRRVPiaKgdB04t89/1O/w1cDnyilFU=
-'
+line_channel_access_token = 'y9AC19uX8VOLcgpFZpJRjx2v9LJ9aDSUCgvHZhnhdijtDbSKhvcayE9hPRwFlCRjUvMVPCZYox1rYMwaekLeEVyJ0gDv9cTA0dGdRyigKk5Qjos+gwUDsxI2H9IP7SpgfKyGmakdqUpI+uRRVPiaKgdB04t89/1O/w1cDnyilFU='
+
 line_channel_secret = '3b1a6196ac1e2c07c215023f0287be4b'
